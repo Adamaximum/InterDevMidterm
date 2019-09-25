db238ae6e9f4aa9e29e90b2d7893bb976fc1c121
Adam Goren <amg1090@nyu.edu>
2019-09-25 18:05:50 -0400

    Basic character movement created

---
+++ Assets/Scripts/MouseLook.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    public float horizontalLookSpeed = 10f;
+    public float verticalLookSpeed = 5f;
+
+    float verticalAngle;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X");
+        float mouseY = Input.GetAxis("Mouse Y");
+
+        transform.parent.Rotate(0f, mouseX * horizontalLookSpeed, 0f);
+
+        verticalAngle -= mouseY * verticalLookSpeed;
+        verticalAngle = Mathf.Clamp(verticalAngle, -80f, 80f);
+
+        transform.localEulerAngles = new Vector3(verticalAngle, transform.localEulerAngles.y, 0f);
+    }
+}
---
+++ Assets/Scripts/RigidbodyMove.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RigidbodyMove : MonoBehaviour
+{
+    Rigidbody myRB;
+    Vector3 myInput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRB = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        myInput = horizontal * transform.right;
+        myInput += vertical * transform.forward;
+    }
+
+    private void FixedUpdate()
+    {
+        myRB.velocity = myInput * 10f;
+    }
+}
