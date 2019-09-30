753ee3e7a9b44b15d35ba2d8929afb66df8a466d
Adam Goren <amg1090@nyu.edu>
2019-09-30 02:23:59 -0400

    Got the jump to work, sort of

--- Assets/Scripts/RigidbodyMove.cs
+++ Assets/Scripts/RigidbodyMove.cs
@@ -7,6 +7,10 @@ public class RigidbodyMove : MonoBehaviour
     Rigidbody myRB;
     Vector3 myInput;
 
+    public float speed = 10f;
+
+    public bool grounded;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -21,10 +25,29 @@ public class RigidbodyMove : MonoBehaviour
 
         myInput = horizontal * transform.right;
         myInput += vertical * transform.forward;
+
+        if (Input.GetKeyDown(KeyCode.Space) && grounded == true)
+        {
+            myRB.AddForce(Vector3.up * 10, ForceMode.Impulse);
+            //speed = 0f;
+            grounded = false;
+        }
     }
 
     private void FixedUpdate()
     {
-        myRB.velocity = myInput * 10f;
+        if (grounded)
+        {
+            myRB.velocity = myInput * speed;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            grounded = true;
+            //speed = 10f;
+        }
     }
 }
