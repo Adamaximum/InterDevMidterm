891ad66b8829cfa780d1de6b378f56c0a7f80357
Adam Goren <amg1090@nyu.edu>
2019-09-30 01:38:51 -0400

    Attempted throw script (spawns but does not apply force)

---
+++ Assets/Scripts/PickUpThrow.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUpThrow : MonoBehaviour
+{
+    public GameObject dodgeball;
+    public Rigidbody dodgeballRB;
+
+    public float speed = 10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(dodgeball, new Vector3(transform.position.x,transform.position.y,transform.position.z), Quaternion.identity);
+            dodgeballRB.AddForce(Vector3.forward * 1000);
+        }
+    }
+}
