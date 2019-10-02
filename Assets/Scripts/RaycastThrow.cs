3157b0dbc0c2a773c1569a7cb2373ed42f5882ca
Adam Goren <amg1090@nyu.edu>
2019-10-02 16:57:49 -0400

    Got a successful build; experimented with a raycast system

---
+++ Assets/Scripts/RaycastThrow.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RaycastThrow : MonoBehaviour
+{
+    public Transform dodgeball;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Ray mouseRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        float mouseRayDist = 10000f;
+
+        RaycastHit rayHit = new RaycastHit();
+
+        Debug.DrawRay(mouseRay.origin, mouseRay.direction * mouseRayDist, Color.magenta);
+
+        if (Physics.Raycast(mouseRay, out rayHit, mouseRayDist))
+        {
+            dodgeball.position = rayHit.point;
+
+            if (Input.GetMouseButton(0))
+            {
+                Instantiate(dodgeball, rayHit.point, dodgeball.rotation);
+            }
+        }
+    }
+}
