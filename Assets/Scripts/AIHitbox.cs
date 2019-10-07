16f2fef0210680c293d7a7990eb1108c67de1438
Adam Goren <amg1090@nyu.edu>
2019-10-07 17:27:14 -0400

    Added some minor hitbox settings to AI

---
+++ Assets/Scripts/AIHitbox.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIHitbox : MonoBehaviour
+{
+    //public GameObject body;
+    //public GameObject head;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //body = GetComponent<GameObject>();
+        //head = GetComponentInChildren<GameObject>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Dodgeball")
+        {
+            if (gameObject.tag == "Body")
+            {
+                Debug.Log("Target is out!");
+            }
+            if (gameObject.name == "Head")
+            {
+                Debug.Log("Headshot! You are out!");
+            }
+        }
+    }
+}
