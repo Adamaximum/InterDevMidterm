f1abe134a799add3462c5b78c7eebe643850cead
Adam Goren <amg1090@nyu.edu>
2019-10-21 10:05:43 -0400

    Programmed in music control, added some music

---
+++ Assets/Scripts/AIMenu.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class AIMenu : MonoBehaviour
+{
+    [Header("AI Movement")]
+    public bool reverse;
+    public float movementSpeed;
+    float movementInput;
+
+    public int movementType;
+    // 0 = No Movement
+    // 1 = Vertical (Z)
+    // 2 = Horizontal (X)
+    public bool randomize;
+    
+    void Start()
+    {
+        if (randomize)
+        {
+            movementType = Random.Range(1, Mathf.RoundToInt(3));
+        }
+    }
+
+    void Update()
+    {
+        AIMovement();
+    }
+    
+    void AIMovement()
+    {
+        if (movementType == 1) // Vert (Z) Movement
+        {
+            transform.position += new Vector3(0f, 0f, movementInput);
+
+            if (transform.position.z >= 14)
+            {
+                reverse = true;
+            }
+            else if (transform.position.z <= 1)
+            {
+                reverse = false;
+            }
+
+            if (reverse)
+            {
+                movementInput = -movementSpeed;
+            }
+            else if (!reverse)
+            {
+                movementInput = movementSpeed;
+            }
+        }
+        else if (movementType == 2) // Horiz (X) Movement
+        {
+            transform.position += new Vector3(movementInput, 0f, 0f);
+
+            if (transform.position.x >= 10)
+            {
+                reverse = true;
+            }
+            else if (transform.position.x <= -9.6f)
+            {
+                reverse = false;
+            }
+
+            if (reverse)
+            {
+                movementInput = -movementSpeed;
+            }
+            else
+            {
+                movementInput = movementSpeed;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/MusicControl.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Code was borrowed from the Unity forums.
+public class MusicControl : MonoBehaviour
+{
+    public AudioClip[] soundtrack;
+
+    public AudioSource source;
+
+    // Use this for initialization
+    void Start()
+    {
+        source = GetComponent<AudioSource>();
+
+        if (!source.playOnAwake)
+        {
+            source.clip = soundtrack[Random.Range(0, soundtrack.Length)];
+            source.Play();
+            Debug.Log("Now Playing: " + source.clip.name);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!source.isPlaying)
+        {
+            source.clip = soundtrack[Random.Range(0, soundtrack.Length)];
+            source.Play();
+            Debug.Log("Now Playing: " + source.clip.name);
+        }
+    }
+}
